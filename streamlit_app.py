b9749dd1f1992cbde0e4d6db706fb56975c140e8
Aviy-Coder <106950551+aviy-coder@users.noreply.github.com>
2022-06-16 18:36:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,16 @@ streamlit.text(fruit_Choice_response.json())
 fruitvicy_normalize=pandas.json_normalize(fruit_Choice_response.json())
 streamlit.dataframe(fruitvicy_normalize)
 
+#Snow to Streamlit connection
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
 
 # streamlit_app.py
 
