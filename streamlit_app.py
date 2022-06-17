655fba96e1eca4dd0334de2ff2755346ef019478
Aviy-Coder <106950551+aviy-coder@users.noreply.github.com>
2022-06-17 12:08:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,26 +56,18 @@ my_cur = my_cnx.cursor()
 #my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
-
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST;")
 my_data_row = my_cur.fetchall()
 streamlit.header("FRUIT_LOAD_LIST Contains")
 streamlit.dataframe(my_data_row)
 
-
 #Add Multi Select List
+AddFruitFromList=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-
-my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-my_fruit_list=my_fruit_list.set_index('Fruit')
-
-fruits_selected=streamlit.multiselect("Pick Some Fruits :", list(my_fruit_list.index),['Avocado','Honeydew'])
-fruits_to_show=my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+AddedFruit=AddFruitFromList.set_index('Fruit')
 
 #New Section to display Fruityvice api response
-streamlit.header('Fruityvice Fruit Advice')
+streamlit.header('Fruityvice Added Fruit')
 
-fruit_Choice=streamlit.text_input('What Fruit would you like information about ?','Kiwi')
-streamlit.write('user asks for',fruit_Choice)
+fruitChoice=streamlit.text_input('What Fruit would you like information about ?')
+streamlit.write('user asks for',fruitChoice)
